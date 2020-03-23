6b54cc786aab4b4aa3588ad03ee15a0273fd0dc0
Dylan Barber <dylanbarber1@outlook.com>
2020-03-23 16:53:27 -0400

    Add node types, body-parser, and listener to app.ts

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+  
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+// const notes = require('./controllers/NoteController');
+
+app.use(bodyParser.json());
+
+// app.use('/', notes);
+
+const port: string | number = process.env.PORT || 3000 ;
+
+app.listen(port, () => {
+    console.log(`Listening on ${port}...`);
+})
