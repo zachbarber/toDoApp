30b4e9a71ef9d191983ccab4a90fde3b511f606a
Zach Barber <zachbarber321@gmail.com>
2020-03-24 15:21:31 -0400

    Add functionality to ToDo Service

--- src/services/IToDoService.ts
+++ src/services/IToDoService.ts
@@ -1,9 +1,9 @@
 import { ToDo } from "../models/ToDo";
 import { FilterOptions } from "../models/FilterOptions";
 
-export interface ToDoService {
+export interface IToDoService {
 
-    create(toDo: Array<ToDo>): Promise<Array<ToDo>>;
+    create(toDo: ToDo): Promise<ToDo>;
     get(filterOptions?: Array<FilterOptions>): Promise<Array<ToDo>>;
     update(toDo: ToDo): Promise<ToDo>;
     delete(Id: number): Promise<void>; 
---
+++ src/services/ToDoService.ts
@@ -0,0 +1,63 @@
+import { IToDoService } from "./IToDoService";
+import { ToDo } from "../models/ToDo";
+import { FilterOptions } from "../models/FilterOptions";
+import { Client } from "pg";
+
+export class ToDoService implements IToDoService {
+
+    private pg = new Client({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'noteTaker',
+        password: 'password',
+        port: 5432,
+    })
+
+    public async create(toDo: ToDo): Promise<ToDo> {
+
+        if (typeof toDo.id !== 'undefined' && toDo.id !== null) {
+
+            throw new Error('Id cannot be supplied on creation');
+        }
+
+        if (typeof toDo.title === 'undefined' || toDo.title === null) {
+
+            throw new Error('Title cannot be blank');
+        }
+
+        if (typeof toDo.body !== 'undefined' && toDo.body !== null) {
+
+            let result = null;
+
+            await this.pg.connect();
+
+            await this.pg
+            .query('INSERT INTO todos (title, body, createdAt) VALUES ($1, $2, now()) RETURNING *', 
+            [toDo.title, toDo.body])
+            .then(res => {
+                result = res.rows[0];
+            });
+
+            return result;
+        } 
+
+        //do query to add just title
+    }
+
+    public async get(filterOptions?: FilterOptions[]): Promise<Array<ToDo>> {
+
+        throw new Error("Method not implemented.");
+    }
+
+    public async update(toDo: ToDo): Promise<ToDo> {
+
+        throw new Error("Method not implemented.");
+    }
+
+    public async delete(Id: number): Promise<void> {
+
+        throw new Error("Method not implemented.");
+    }
+
+
+}
